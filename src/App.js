c3e8f9c08f4f500b523a7dda2da0573ba5299577
harisasif8 <hasif@nisum.com>
2022-05-25 18:40:25 +0500

    counter app first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
 import './App.css';
 
 function App() {
+  
+  const [count, setCount] = useState(4)
+  const [incBtn,setIncBtn] = useState(false)
+  const [decBtn,setDecBtn] = useState(false)
+
+  const decrementCount= () => {
+    
+    setCount(prevCount => prevCount - 1)
+    if(count<=1){
+      setDecBtn(true)
+    }
+    else{
+      setDecBtn(false)
+    }
+  }
+
+  
+
+  const incrementCount= () => {
+    setCount(prevCount => prevCount + 1)
+    if(count>=9){
+      setIncBtn(true)
+    }
+    else{
+      setIncBtn(false)
+    }
+  }
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App ">
+      <div className=' container  bg-secondary w-25  p-4'>
+            <button className='btn btn-danger m-3 w-25' disabled={decBtn} onClick={decrementCount}>-</button>
+            <span className='text-white '>{count}</span>
+            <button className='btn btn-success m-3 w-25'  disabled={incBtn} onClick={incrementCount}>+</button>
+      </div>
     </div>
   );
 }
