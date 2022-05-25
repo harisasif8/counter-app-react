6c3fa6b0d191d85c22f53d4a93dadbcb45a8d286
harisasif8 <hasif@nisum.com>
2022-05-25 21:50:07 +0500

    counter app using single state

--- src/App.js
+++ src/App.js
@@ -1,43 +1,40 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
-  
-  const [count, setCount] = useState(4)
-  const [incBtn,setIncBtn] = useState(false)
-  const [decBtn,setDecBtn] = useState(false)
 
-  const decrementCount= () => {
-    
+  const [count, setCount] = useState(0)
+
+
+  const decrementCount = () => {
+
     setCount(prevCount => prevCount - 1)
-    if(count<=1){
-      setDecBtn(true)
-    }
-    else{
-      setDecBtn(false)
-    }
+
+
   }
 
-  
 
-  const incrementCount= () => {
+
+  const incrementCount = () => {
+
     setCount(prevCount => prevCount + 1)
-    if(count>=9){
-      setIncBtn(true)
-    }
-    else{
-      setIncBtn(false)
-    }
+
   }
 
-  
+
+
+
 
   return (
-    <div className="App ">
-      <div className=' container  bg-secondary w-25  p-4'>
-            <button className='btn btn-danger m-3 w-25' disabled={decBtn} onClick={decrementCount}>-</button>
-            <span className='text-white '>{count}</span>
-            <button className='btn btn-success m-3 w-25'  disabled={incBtn} onClick={incrementCount}>+</button>
+    <div className="App">
+      <div className=' container  bg-secondary p-4 container-body'>
+        <div className='row '>
+          <div className='col'>
+        <button className='btn btn-danger m-3 w-25' disabled={count===0} onClick={decrementCount}>-</button>
+        <span className='text-white'>{count}</span>
+        <button className='btn btn-success m-3 w-25' disabled={count===10} onClick={incrementCount}>+</button>
+        </div>
+        </div>
       </div>
     </div>
   );
